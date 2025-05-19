e40b17a06241caccc56e18eaf614f969d8b12ea6
sharjeel <sharjeelkhatri303@gmail.com>
2025-05-19 20:49:35 +0500

    Update index.js

--- index.js
+++ index.js
@@ -13,9 +13,7 @@ app.use(
   cors({
     origin: "*",
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
+    })
 );
 app.use(express.json());
 
