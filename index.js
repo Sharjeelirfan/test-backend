0e9367a1a47e4e46bedff12e3ac738f85b8fd4bd
sharjeel <sharjeelkhatri303@gmail.com>
2025-05-19 20:47:58 +0500

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const prisma = new PrismaClient();
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "*",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
